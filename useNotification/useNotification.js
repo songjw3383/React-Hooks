22d486260b696a5e94b0122d413e364b050a787a
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-02-12 00:39:42 +0900

    Finished

---
+++ useNotification/useNotification.js
@@ -0,0 +1,22 @@
+export const useNotification = (title, options) => {
+    if(!("Notification" in window)) {
+      return;
+    }
+    useEffect(() => {
+  
+    })
+    const fireNotif = () => {
+      if(Notification.permission !== "granted"){
+        Notification.requestPermission().then(permission => {
+          if(permission == " granted") {
+            new Notification(title, options)
+          } else {
+            return;
+          }
+        })
+      } else {
+        new Notification(title, options)
+      }
+    };
+    return fireNotif;
+  } 
